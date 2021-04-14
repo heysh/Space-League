be3180a44047a1c289e1e49b773798bae9b415e0
Harshil Surendralal <bf000259@reading.ac.uk>
2021-04-14 11:34:34 +0100

    Added animation to splash screen background

--- app/src/main/java/com/bf000259/spaceleague/MainActivity.java
+++ app/src/main/java/com/bf000259/spaceleague/MainActivity.java
@@ -2,12 +2,39 @@ package com.bf000259.spaceleague;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.ValueAnimator;
 import android.content.Intent;
 import android.os.Bundle;
+import android.renderscript.Sampler;
 import android.view.View;
 import android.view.WindowManager;
+import android.view.animation.LinearInterpolator;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private void animateBackground() {
+        final ImageView bg1 = (ImageView) findViewById(R.id.backgroundOne);
+        final ImageView bg2 = (ImageView) findViewById(R.id.backgroundTwo);
+
+        final ValueAnimator animator = ValueAnimator.ofFloat(0.0f, 1.0f);
+
+        animator.setRepeatCount(ValueAnimator.INFINITE);
+        animator.setInterpolator(new LinearInterpolator());
+        animator.setDuration(10000L);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                final float progress = (float) animation.getAnimatedValue();
+                final float width = bg1.getWidth();
+                final float translationX = width * progress;
+
+                bg1.setTranslationX(-translationX);
+                bg2.setTranslationX(-(translationX - width));
+            }
+        });
+        animator.start();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +43,8 @@ public class MainActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_main);
+
+        animateBackground();
 
         findViewById(R.id.play).setOnClickListener(new View.OnClickListener() {
             @Override
