71d977b650a6bfecc9790210e74b9d9fe3bf9e48
Harshil Surendralal <bf000259@reading.ac.uk>
2021-04-14 17:03:54 +0100

    Removed redundant class variable and methods

--- app/src/main/java/com/bf000259/spaceleague/Enemy.java
+++ app/src/main/java/com/bf000259/spaceleague/Enemy.java
@@ -1,13 +1,4 @@
 package com.bf000259.spaceleague;
 
 public class Enemy extends Object {
-    protected int speed;
-
-    public int getSpeed() {
-        return speed;
-    }
-
-    public void setSpeed(int speed) {
-        this.speed = speed;
-    }
 }
