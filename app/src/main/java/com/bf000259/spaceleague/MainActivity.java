280415417d03e31f5973f4d5376979464ea09f17
Harshil Surendralal <bf000259@reading.ac.uk>
2021-04-19 13:21:40 +0100

    Removed unused imports

--- app/src/main/java/com/bf000259/spaceleague/MainActivity.java
+++ app/src/main/java/com/bf000259/spaceleague/MainActivity.java
@@ -3,12 +3,10 @@ package com.bf000259.spaceleague;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.InputType;
-import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
